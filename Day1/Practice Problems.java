416b355033e77a7d1876daf64d5717863f1bb3f9
Priyanshi Naredi <66825094+priyanshinaredi@users.noreply.github.com>
2022-06-01 20:35:15 +0530

    Add files via upload

---
+++ Day1/Practice Problems.java
@@ -0,0 +1,108 @@
+package javaBasic;
+
+import java.util.Scanner;
+
+public class practiceprob {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1
+//		int a,b;
+		Scanner input= new Scanner(System.in);
+//		System.out.println("Enter 2 num: ");
+//		a= input.nextInt();
+//		b= input.nextInt();
+//		System.out.println("Product of numbers: "+a*b);
+		
+		//2
+//		int radi;
+//		float area,perim;
+//		System.out.println("Enter the radius");
+//		radi = input.nextInt();
+//		
+//		area= (float) (3.14*radi*radi);
+//		perim = (float) (2*3.14*radi);
+//		System.out.println("Area: "+ area +" Permiter: "+perim);
+		
+		//3
+//		int inch = 0;
+//		float meter;
+//		System.out.println("Enter inches: ");
+//		inch = input.nextInt();
+//		meter= (float)(0.0254*inch);
+//		System.out.println("Inches in meters: "+ meter);
+		
+		//4
+		
+//		int a,b,c;
+//		System.out.println("Enter 3 numbers: ");
+//		a= input.nextInt();
+//		b= input.nextInt();
+//		c= input.nextInt();
+//		
+//		if (a>b) {
+//			if (a>c) {
+//				System.out.println("A is greatest");
+//			}
+//		}
+//		if (b>c) {
+//			if (b>a) {
+//				System.out.println("B is greatest");
+//			}
+//		}
+//		else {
+//			System.out.println("C is greatest");
+//		}
+		
+		//5
+		
+//		int year;
+//		System.out.println("Enter the year: ");
+//		year= input.nextInt();
+//		if(year%4==0) {
+//			if(year%100==0) {
+//				System.out.println("A leap year");
+//			}
+//		}
+//		else {
+//			System.out.println("Not a leap year");
+//		}
+		
+		//6
+		
+//		int a,b,temp;
+//		System.out.println("Enter 2 numbers:");
+//		a= input.nextInt();
+//		b= input.nextInt();
+//		temp=a;
+//		a=b;
+//		b=temp;
+//		System.out.println("a is "+ a+" B is "+b);
+		
+		//7
+		
+//		int marks;
+//		System.out.println("Enter marks");
+//		marks = input.nextInt();
+//		if (marks>=90){
+//		System.out.println("A++");
+//		}
+//		else if(marks>=80) {
+//			System.out.println("A");
+//		}
+//		else if(marks>=50) {
+//			System.out.println("B");
+//		}
+//		else {
+//			System.out.println("F");
+//		}
+//		
+//		
+		
+		
+		
+		
+
+	}
+
+}
