b734fadf3a89e735bdf84bd38c3084b95314e760
Priyanshi Naredi <66825094+priyanshinaredi@users.noreply.github.com>
2022-06-03 00:31:43 +0530

    Add files via upload

---
+++ Day2/Practice_Problem.java
@@ -0,0 +1,127 @@
+package javaBasic2;
+import java.util.*;
+public class Practice_Problem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1
+		
+//		int[] array= {1,2,3,4,5};
+//		int sum=0;
+//		for(int i=0;i<5;i++) {
+//			sum+=array[i];
+//		}
+//		int avg=sum/5;
+//		System.out.println("Average of array is: "+avg);
+//		for(int i=0;i<5;i++) {
+//			if(array[i]>avg) {
+//				System.out.println("Number greater than avg in array is: "+array[i]);
+//			}
+//		}
+		//2
+		
+		Scanner input = new Scanner(System.in);
+//		System.out.println("Enter table of:");
+//		int n= input.nextInt();
+//		for(int i=0;i<10;i++) {
+//			System.out.println(n*(i+1));
+//		}
+		
+		//3
+//		int[] array= {1,2,3,4,5};
+//		int sum=0;
+//		for(int i=0;i<5;i++) {
+//			sum+=array[i];
+//		}
+//		System.out.println("Sum of the array is: "+sum);
+//				
+		//4
+//		int[] array= {1,2,3,4,5};
+//		int max = array[0];
+//		int min= array[0];
+//		
+//		for(int i=0;i<5;i++) {
+//			 if (array[i] > max) {
+//	                max = array[i];
+//	            }
+//	 
+//	            // if the current element is smaller than the minimum found so far
+//	            else if (array[i] < min) {
+//	                min = array[i];
+//	            }
+//		}
+//		System.out.println("Maximum is: "+max);
+//		System.out.println("Minimum is: "+min);
+		
+		//5
+//		int[] array= {0,1,0,1,0};
+//		int[] array2= new int[array.length];
+//		int num=0;
+//		for(int i=0;i<array.length;i++) {
+//			if(array[i]==0) {
+//				num++;
+//			}
+//		}
+//		for(int i=0;i<num;i++) {
+//			array2[i]=0;
+//		}
+//		for(int i=0;i<array2.length-num;i++) {
+//			array2[num+i]=1;
+//		}
+//		for(int i=0;i<5;i++) {
+//			System.out.print(array[i]+" ");
+//		}
+//		System.out.println();
+//		for(int i=0;i<5;i++) {
+//			System.out.print(array2[i]+" ");
+//		}
+		
+		//6
+		
+//		int a,b,c;
+//		System.out.println("Enter 3 numbers: ");
+//		a= input.nextInt();
+//		b= input.nextInt();
+//		c= input.nextInt();
+//		
+//		if (a<b) {
+//			if (a<c) {
+//				System.out.println(a);
+//			}
+//		}
+//		if (b<c) {
+//			if (b<a) {
+//				System.out.println(b);
+//			}
+//		}
+//		else {
+//			System.out.println(c);
+//		}
+		//7
+//		System.out.println("Enter a number: ");
+//		int n=input.nextInt();
+//		int a1=0;
+//		int a2=1;
+//		int a3;
+//		System.out.print(a1+ " "+a2+" ");
+//		for (int i=1;i<=n;i++) {
+//			a3=a2+a1;
+//			System.out.print(a3+" ");
+//			a1=a2;
+//			a2=a3;
+//			
+//		}
+		
+		//8
+//		System.out.println("Enter a number: ");
+//		int n=input.nextInt();
+//		int fact=1;
+//		for(int i=1;i<=n;i++) {
+//			fact= fact*i;
+//		}
+//		System.out.println("Factorial value of "+n+" is "+fact);
+//		
+
+	}
+
+}
